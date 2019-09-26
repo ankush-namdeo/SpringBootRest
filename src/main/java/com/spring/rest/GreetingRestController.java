e7647c0be0e2c2c529d55691aed2a207b8552dba
ankush-namdeo <55854305+ankush-namdeo@users.noreply.github.com>
2019-09-26 23:30:52 +0200

    Updated files

--- src/main/java/com/spring/rest/GreetingRestController.java
+++ src/main/java/com/spring/rest/GreetingRestController.java
@@ -1,5 +1,7 @@
 package com.spring.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,22 +20,19 @@ public class GreetingRestController {
 
 	@RequestMapping("/greeting")
 	@GetMapping
-	public Response greeting(@RequestParam(value = "id", required = false) Integer id,
+	public ResponseEntity<Object> greeting(@RequestParam(value = "id", required = false) Integer id,
 			@RequestParam(value = "account") accounttype account,
 			@RequestParam(value = "type", required = false) sizeType size) {
-		Response response = new Response();
+
 		if (null != id && account.equals(accounttype.business)) {
-			response.setMessage("Hi, userId " + id);
-			return response;
-		} else if (account.equals(accounttype.business) && size.equals(sizeType.small)) {
-			response.setError("path not set");
-			return response;
+
+			return new ResponseEntity<Object>("Hi, userId " + id, HttpStatus.OK);
 		} else if (account.equals(accounttype.business) && size.equals(sizeType.big)) {
-			response.setMessage("Welcome, business user!");
-			return response;
-		} else {
-			response.setError("path not set");
-			return response;
+			return new ResponseEntity<Object>("Welcome, business user!", HttpStatus.OK);
+		} else if (account.equals(accounttype.business) && size.equals(sizeType.small)) {
+			return new ResponseEntity<Object>(HttpStatus.NOT_IMPLEMENTED);
 		}
+
+		return new ResponseEntity<Object>(HttpStatus.NOT_IMPLEMENTED);
 	}
 }
--- src/test/java/com/example/demo/SpringBootRestApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootRestApplicationTests.java
@@ -1,81 +1,82 @@
 package com.example.demo;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
+import org.mockito.InjectMocks;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.spring.rest.GreetingRestController;
-import com.spring.rest.Response;
-import com.spring.rest.SpringBootRestApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { GreetingRestController.class,
-		SpringBootRestApplication.class }, webEnvironment = WebEnvironment.RANDOM_PORT)
-
 public class SpringBootRestApplicationTests {
-	@LocalServerPort
-	int randomPort;
-
-	@Test
-	public void testGreetingsWithIDandAccount() throws URISyntaxException {
-		RestTemplate r = new RestTemplate();
-		Integer id = 123;
-		String account = "business";
-		String type = "small";
-		final String baseUrl = "http://localhost:" + randomPort + "/greeting?id=" + id + "&account=" + account
-				+ "&type=" + type;
-		URI uri = new URI(baseUrl);
-		ResponseEntity<Response> result = r.getForEntity(uri, Response.class);
-		Assert.assertEquals(200, result.getStatusCodeValue());
-		Assert.assertEquals("Hi, userId " + id, result.getBody().getMessage());
-	}
-
-	@Test
-	public void testGreetingsWithAccountandTypePositive() throws URISyntaxException {
-		RestTemplate r = new RestTemplate();
-		String account = "business";
-		String type = "big";
-		final String baseUrl = "http://localhost:" + randomPort + "/greeting?&account=" + account + "&type=" + type;
-		URI uri = new URI(baseUrl);
-		ResponseEntity<Response> result = r.getForEntity(uri, Response.class);
-		Assert.assertEquals(200, result.getStatusCodeValue());
-		Assert.assertEquals("Welcome, business user!", result.getBody().getMessage());
-	}
-
-	@Test
-	public void testGreetingsWithAccountandTypeSmall() throws URISyntaxException {
-		RestTemplate r = new RestTemplate();
-		String account = "business";
-		String type = "small";
-		final String baseUrl = "http://localhost:" + randomPort + "/greeting?&account=" + account + "&type=" + type;
-		URI uri = new URI(baseUrl);
-		ResponseEntity<Response> result = r.getForEntity(uri, Response.class);
-		Assert.assertEquals(200, result.getStatusCodeValue());
-		Assert.assertEquals("path not set", result.getBody().getError());
+	
+	private MockMvc mockMvc;
+	
+	@InjectMocks
+	private GreetingRestController greetingRestController;
+	
+	@Before
+	public void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.standaloneSetup(greetingRestController).build();
 
 	}
 	
 	@Test
-	public void testGreetingsPersonalAccount() throws URISyntaxException {
-		RestTemplate r = new RestTemplate();
-		String account = "personal";
-		String type = "small";
-		final String baseUrl = "http://localhost:" + randomPort + "/greeting?&account=" + account + "&type=" + type;
-		URI uri = new URI(baseUrl);
-		ResponseEntity<Response> result = r.getForEntity(uri, Response.class);
-		Assert.assertEquals(200, result.getStatusCodeValue());
-		Assert.assertEquals("path not set", result.getBody().getError());
-
+	public void testGreetingForId() throws Exception {
+		
+		mockMvc.perform(get("/greeting?id=123&account=business"))
+		.andExpect(status().isOk()).andExpect(content().string("Hi, userId 123"));
+		
 	}
+	
+	@Test
+	public void testGreetingForAccountAndSize() throws Exception {
+		
+		mockMvc.perform(get("/greeting?account=business&type=big"))
+		.andExpect(status().isOk()).andExpect(content().string("Welcome, business user!"));
+		
+	}
+	
+	@Test
+	public void testGreetingForAccountAndSizeError() throws Exception {
+		
+		mockMvc.perform(get("/greeting?account=business&type=small"))
+		.andExpect(status().isNotImplemented());
+		
+	}
+	
+	@Test
+	public void testGreetingForAccountAndSizeErrorInRequest() throws Exception {
+		
+		mockMvc.perform(post("/greeting?account=business&type=small"))
+		.andExpect(status().isNotImplemented());
+		
+	}
+	
+	@Test
+	public void testGreetingForNoAccountAndSize() throws Exception {
+		
+		mockMvc.perform(post("/greeting?account=personal&type=small"))
+		.andExpect(status().isNotImplemented());
+		
+	}
+	@Test
+	public void testGreetingForFaultyRequest() throws Exception {
+		
+		mockMvc.perform(post("/greeting?account=persal&type=small"))
+		.andExpect(status().isBadRequest());
+		
+	}
+	
+	
 	
 	
 }
